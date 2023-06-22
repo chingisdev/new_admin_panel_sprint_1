ad430a5bc366fcd0ee8405fbe8ad224207d3798c
Chingis <chingisdev@gmail.com>
2023-06-22 20:53:40 +0300

    described model dataclasses

---
+++ sqlite_to_postgres/models.py
@@ -0,0 +1,50 @@
+import uuid
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass(frozen=True)
+class FilmWork:
+    id: uuid.UUID
+    title: str
+    description: str
+    creation_date: datetime
+    file_path: str
+    rating: float
+    type: str
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass(frozen=True)
+class Genre:
+    id: uuid.UUID
+    name: str
+    description: str
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass(frozen=True)
+class Person:
+    id: uuid.UUID
+    full_name: str
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass(frozen=True)
+class GenreFilmwork:
+    id: uuid.UUID
+    film_work_id: uuid.UUID
+    genre_id: uuid.UUID
+    created_at: datetime
+
+
+@dataclass(frozen=True)
+class PersonFilmwork:
+    id: uuid.UUID
+    film_work_id: uuid.UUID
+    person_id: uuid.UUID
+    role: str
+    created_at: datetime
